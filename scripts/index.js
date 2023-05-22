485e5696ba09af5c1529b2329e76e01cecd90055
5dt1 <contact@5dt1.com>
2023-05-22 23:24:46 +0200

    Some advancement -Finalized index.html -Started about.html

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,13 @@ window.addEventListener('DOMContentLoaded', function() {
         window.open('https://github.com/cinqdtun/thetitanbot', "_blank");
     });
     document.querySelector("#github-project-4-btn").addEventListener('click', () =>{
-        window.open('https://github.com/cinqdtun/ESM', "_blank");
+        window.open('https://5dt1.com', "_blank");
     });
+
+    setTimeout(() => {
+        const elementsToFade = document.querySelectorAll('.left-container, .right-container');
+        for (let i = 0; i < elementsToFade.length; i++) {
+            elementsToFade[i].classList.add('show');
+        }
+    }, 500);
 });

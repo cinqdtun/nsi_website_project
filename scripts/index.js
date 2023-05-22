a514586cc5652631473b5f53c3ef22324265956f
5dt1 <dehanflavian2@gmail.com>
2023-05-22 16:32:22 +0200

    Monday 22nd progression -Advancement of webpage

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', function() {
+    document.querySelector("#github-project-1-btn").addEventListener('click', () =>{
+        window.open('https://github.com/cinqdtun/youtube_music_app', "_blank");
+    });
+    document.querySelector("#github-project-2-btn").addEventListener('click', () =>{
+        window.open('https://github.com/cinqdtun/nsi_website_project', "_blank");
+    });
+    document.querySelector("#github-project-3-btn").addEventListener('click', () =>{
+        window.open('https://github.com/cinqdtun/thetitanbot', "_blank");
+    });
+    document.querySelector("#github-project-4-btn").addEventListener('click', () =>{
+        window.open('https://github.com/cinqdtun/ESM', "_blank");
+    });
+});

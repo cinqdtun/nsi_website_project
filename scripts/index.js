abc579120c1dc33a054f0e9d7859e92c4a4f4e02
5dt1 <dehanflavian2@gmail.com>
2023-05-23 16:08:42 +0200

    Tuesday 22nd progression -Advancement of webpage

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
 window.addEventListener('DOMContentLoaded', function() {
+    history.pushState(null, "", location.href.split("#")[0]);
+
     document.querySelector("#github-project-1-btn").addEventListener('click', () =>{
         window.open('https://github.com/cinqdtun/youtube_music_app', "_blank");
     });
@@ -12,10 +14,15 @@ window.addEventListener('DOMContentLoaded', function() {
         window.open('https://5dt1.com', "_blank");
     });
 
-    setTimeout(() => {
+    setTimeout(async () => {
         const elementsToFade = document.querySelectorAll('.left-container, .right-container');
         for (let i = 0; i < elementsToFade.length; i++) {
             elementsToFade[i].classList.add('show');
+            await sleep(1000)
         }
     }, 500);
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 });
